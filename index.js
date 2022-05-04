475274ba4a3060f9c02a1bb10d9bfe1eb6871bee
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-05-04 14:05:40 -0400

    Last test to figure out

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Add your functions here
+
+
+function map(array, callback) {
+    let newArray = []
+    // for (let i = 0; newArray.length; i++) {
+    //     return functionJob(newArray[i])
+    // }
+
+    array.forEach((value, index) => {
+        newArray.push(callback(array[index]))
+    })
+
+
+    return newArray
+   
+}
+
+sourceArray = [1,2,3]
+
+function reduce(SourceArray, callback, startingPoint ) {
+
+    let total 
+    if (startingPoint) {
+        total = startingPoint
+
+        SourceArray.forEach((value, index) => {
+            total = callback(SourceArray[index], total )
+        })
+        return total
+    } else {
+  
+        total = SourceArray[0]
+
+        SourceArray.forEach((value, index) => {
+            total = callback(SourceArray[index], total )
+        })
+        return total
+    }
+}
+
+
+
+
+
